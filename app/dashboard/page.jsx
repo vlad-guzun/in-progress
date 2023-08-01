44d16b46b64f018bf3de0932f93765124c47e8dc
vladguzun <vladguzun20022@gmail.com>
2023-08-01 09:06:11 -0700

    commit

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -10,7 +10,7 @@ const Dashboard = () => {
   useEffect(() => {
     const getPrompts = async () => {
       try {
-        const response = await axios.get('/api/all-prompts')
+         await axios.get('/api/all-prompts/')
         .then((data) => {
           setPrompts(data.data.prompts)
         })
@@ -20,7 +20,7 @@ const Dashboard = () => {
       }
     };
 
-    getPrompts();
+    getPrompts()
   }, []);
 
   return (
